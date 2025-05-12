7458b40f8e2a424bd94bffd639e594d485c89bad
Agastyasourcecodes <agastyamaurya123@gmail.com>
2025-05-12 14:21:35 +0530

    console logic to get computer and users data

---
+++ index.js
@@ -0,0 +1,25 @@
+function getcomputerchoice() {
+
+    const random=Math.floor(Math.random()*3)+1;
+    return random;
+}
+
+console.log(getcomputerchoice());
+
+
+function gethumanchoice(){
+
+
+parseInt(prompt("enter ur choice in 1 2 3"))
+return prompt;
+
+}
+console.log(gethumanchoice());
+
+function playround{
+
+
+
+
+
+}
